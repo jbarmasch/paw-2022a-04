0f7d27d0621c536486523e450bb81f8e370c6e14
ebellver <ebellver@itba.edu.ar>
2022-04-08 12:59:45 -0300

    Update schema.sql
    Co-authored-by: Santiago Lo Coco <slococo@itba.edu.ar>
    Co-authored-by: Juan Barmasch <jbarmasch@itba.edu.ar>

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -12,5 +12,29 @@ CREATE TABLE IF NOT EXISTS events (
     location VARCHAR(100),
     maxCapacity INTEGER NOT NULL,
     price DOUBLE PRECISION NOT NULL,
+    date TIMESTAMP NOT NULL,
     CHECK (location IN ('Adrogué', 'San Isidro', 'Don Torcuato', 'Belgrano', 'Recoleta', 'Turdera'))
+);
+
+CREATE TABLE IF NOT EXISTS eventTags (
+    eventId INTEGER REFERENCES events,
+    tag VARCHAR(100),
+    PRIMARY KEY (eventId, tag)
+);
+
+CREATE TABLE IF NOT EXISTS tickets (
+    eventId INTEGER REFERENCES events,
+    name VARCHAR(100),
+    price DOUBLE PRECISION,
+    booked INTEGER,
+    maxQty INTEGER,
+    PRIMARY KEY (eventId, name)
+);
+
+CREATE TABLE IF NOT EXISTS bookings (
+    userId INTEGER REFERENCES users,
+    eventId INTEGER REFERENCES events,
+    name VARCHAR(100),
+    qty INTEGER,
+    PRIMARY KEY (userId, eventId, name)
 );
