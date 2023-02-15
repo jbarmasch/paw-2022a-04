a59c51953d8f3f43f49bab90f47544413464510f
Santiago Lo Coco <slococo@itba.edu.ar>
2023-02-15 22:57:35 +0100

    Fix server bug
    Co-authored-by: Ezequiel Bellver <ebellver@itba.edu.ar>
    Co-authored-by: Juan Barmasch <jbarmasch@itba.edu.ar>

--- frontend/src/utils/server.js
+++ frontend/src/utils/server.js
@@ -1,4 +1,5 @@
-export const server = 'http://pawserver.it.itba.edu.ar/paw-2022a-04';
+// export const server = 'http://pawserver.it.itba.edu.ar/paw-2022a-04';
+export const server = 'http://localhost:2555/paw-2022a-04';
 
 export const fetcher = async url => {
     const res = await fetch(url)
